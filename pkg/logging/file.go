8330dd15061b4df028077fcd2f1cb497663f4fa2
SunLightLei <1255193087@qq.com>
2022-03-03 21:40:20 +0800

    增加log-first

---
+++ pkg/logging/file.go
@@ -0,0 +1,24 @@
+package logging
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	LogSavePath = "runtime/logs/"
+	LogSaveName = "log"
+	LogFileExt  = "log"
+	TimeFormat  = "20060102"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
+
+func getLogFileFullPath() string {
+	prefixPath := getLogFilePath()
+	suffixPath := fmt.Sprintf("%s%s.%s", LogSaveName, time.Now().Format(TimeFormat), LogFileExt)
+
+	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+}
