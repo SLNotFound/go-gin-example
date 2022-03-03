a3e1212d7db0dc2335b3484e535e6b8e73caebc9
SunLightLei <1255193087@qq.com>
2022-03-03 08:07:50 +0800

    addTag的路由逻辑

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,12 +1,14 @@
 package v1
 
 import (
+	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
 	"go-gin-example/models"
 	"go-gin-example/pkg/e"
 	"go-gin-example/pkg/setting"
 	"go-gin-example/pkg/util"
+	"log"
 	"net/http"
 )
 
@@ -41,7 +43,39 @@ func GetTags(c *gin.Context) {
 	})
 }
 func AddTag(c *gin.Context) {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
 
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("名称不能为空")
+	valid.MaxSize(name, 100, "name")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	code := e.INVALID_PARAMS
+	//if !valid.HasErrors() {
+	//	if !models.ExistTagByName(name) {
+	//		code = e.SUCCESS
+	//		models.AddTag(name, state, createdBy)
+	//	} else {
+	//		code = e.ERROR_EXIST_TAG
+	//	}
+	//}
+	if valid.HasErrors() {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	} else {
+		code = e.SUCCESS
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 func EditTag(c *gin.Context)   {}
 func DeleteTag(c *gin.Context) {}
