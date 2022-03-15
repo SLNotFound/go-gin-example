82d44c177f37392260b4e4c0c76d4a756e093fc1
SunLightLei <1255193087@qq.com>
2022-03-15 21:28:22 +0800

    上传文件

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/fvbock/endless"
 	"go-gin-example/models"
 	"go-gin-example/pkg/logging"
 	"go-gin-example/pkg/setting"
 	"go-gin-example/routers"
-	"log"
-	"syscall"
 )
 
 func main() {
@@ -16,22 +13,24 @@ func main() {
 	models.Setup()
 	logging.Setup()
 
-	endless.DefaultReadTimeOut = setting.ServerSetting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.ServerSetting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
+	router := routers.InitRouter()
+
+	//endless.DefaultReadTimeOut = setting.ServerSetting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.ServerSetting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
 	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
-
+	router.Run(endPoint)
 	// endless.NewServer 返回一个初始化的 endlessServer 对象
-	server := endless.NewServer(endPoint, routers.InitRouter())
+	//server := endless.NewServer(endPoint, routers.InitRouter())
 	// 在BeforeBegin 时输出当前进程的 pid ，调用 ListenAndServe 将实际“启动”服 务
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
-	}
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err: %v", err)
-	}
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
 
 	//r := routers.InitRouter()
 	//
